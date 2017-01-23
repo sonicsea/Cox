b50986ef711fcfbb9dfc32055532aa184b0d1c72
caitayl <taylor.cai@verizonwireless.com>
2017-01-22 20:27:48 -0500

    add email class and PDF feature

--- Cox/Controllers/ReportController.cs
+++ Cox/Controllers/ReportController.cs
@@ -5,25 +5,62 @@ using System.Web;
 using System.Web.Mvc;
 using Cox.ViewModels;
 using Cox.Helpers;
+using Rotativa;
+using log4net;
 
 namespace Cox.Controllers
 {
     public class ReportController : Controller
     {
+        private static log4net.ILog Log { get; set; }
+        ILog log = log4net.LogManager.GetLogger(typeof(CategoryController));
         // GET: Report
         public ActionResult Index()
         {
-            if (Session["UserID"] == null) RedirectToAction("Login", "Home");
+            if (Session["UserID"] == null)
+            {
+                log.Error("Unauthrized access to the report page.");
+                return RedirectToAction("Login", "Home");
+            }
+            try
+            {
+                int userID = Convert.ToInt32(Session["UserID"]);
+
+
+                int specialTaskID = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["Special_Task_ID"].ToString());
+
+                ReportViewModel reportInfo = CoxLogic.GetReport(userID, specialTaskID);
+
+
+
+                return View(reportInfo);
+            }
+            catch(Exception ex)
+            {
+                log.Error(ex.Message, ex);
+                return null;
+            }
+        }
+
+        public ActionResult PDF(ReportViewModel pdfReportInfo)
+        {
+
+            return View(pdfReportInfo);
+        }
+
+        [HttpPost]
+        [MultipleButton(Name = "action", Argument = "Download")]
+        public ActionResult DownloadPDF()
+        {
             int userID = Convert.ToInt32(Session["UserID"]);
-            
+
 
             int specialTaskID = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["Special_Task_ID"].ToString());
 
             ReportViewModel reportInfo = CoxLogic.GetReport(userID, specialTaskID);
 
-            
-
-            return View(reportInfo);
+            return new ViewAsPdf("PDF", reportInfo) { FileName = "MyReport.pdf" };
         }
+
     }
 }
